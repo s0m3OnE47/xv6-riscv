c77308bab6e14ee4c025b7309e2b9b066dadcb0c
Aniket Dhere <aniket.dhere@gmail.com>
2020-03-09 19:35:45 -0400

    uthread completed

--- user/uthread.c
+++ user/uthread.c
@@ -10,9 +10,27 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+	uint64 ra;
+	uint64 sp;
+	uint64 s0;
+	uint64 s1;
+	uint64 s2;
+	uint64 s3;
+	uint64 s4;
+	uint64 s5;
+	uint64 s6;
+	uint64 s7;
+	uint64 s8;
+	uint64 s9;
+	uint64 s10;
+	uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -57,10 +75,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch((uint64)&t->context,(uint64)&next_thread->context);
   } else
     next_thread = 0;
 }
@@ -75,6 +90,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)(t->stack+STACK_SIZE);
 }
 
 void 
@@ -153,7 +170,9 @@ main(int argc, char *argv[])
   a_started = b_started = c_started = 0;
   a_n = b_n = c_n = 0;
   thread_init();
+  printf("1\n");
   thread_create(thread_a);
+  printf("2\n");
   thread_create(thread_b);
   thread_create(thread_c);
   thread_schedule();
